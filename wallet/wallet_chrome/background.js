c641a69faf0df1084b106891844b2a95f37240c9
aaa3531 <aaa3531@naver.com>
2022-10-14 19:58:24 +0900

    익스텐션 틀 커밋

---
+++ wallet/wallet_chrome/background.js
@@ -0,0 +1,32 @@
+// let color = '#3aa757';
+
+
+
+// let value = 0;
+// chrome.storage.local.set({key: value}, function() {
+//   alert('Value is set to ' + value);
+// });
+console.log('');
+// chrome.storage.local.get('mnemonic', function(result) {
+//   console.log(result);
+//   console.log('저장된 니모닉 ' + result.mnemonic);
+  
+//   //저장된 니모닉이 없다면
+//   if(!result.mnemonic) {
+//     let value = 1;
+//     chrome.storage.local.set({'mnemonic': value}, function() {
+//       console.log('Value is set to ' + value);
+//     });
+//   } else {
+//     chrome.storage.local.set({'mnemonic': Number(result.mnemonic) + 1}, function() {
+//       console.log('값 증가' + (Number(result.mnemonic) + 1));
+//     });
+//   }
+// });
+
+// chrome.storage.local.get(['key'], function(result) {
+//   console.log(result.key);
+//   // if(!result.key) {
+//   //   alert('키값이 존재하지 않음');
+//   // }
+// });
