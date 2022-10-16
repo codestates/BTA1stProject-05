06758d90db2daad40a5f1432bb17b4fb4c862d7c
Woo-Jin Han <verecahill@gmail.com>
2022-10-16 18:19:47 +0900

    addresslist key 추가
    addresslist key 추가

--- client/chrome-ext-react/src/Components/AddressList.js
+++ client/chrome-ext-react/src/Components/AddressList.js
@@ -6,16 +6,16 @@ function AddressList(props) {
   const addresses = props.addresses;
   const addressList = addresses.map((address, index) => {
     return (
-      <>
+      <div key={address.address}>
         <div>
           <b>Address {index + 1}</b>
         </div>
-        <ListItem key={address.address} disablePadding>
+        <ListItem disablePadding>
           <ListItemText className="address" primary={address.address} />
         </ListItem>
         <div>Balance : {address.balance}</div>
         <hr />
-      </>
+      </div>
     );
   });
   return <List>{addressList}</List>;
